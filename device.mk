57718f41d430e19927248cd72fbe6e41d9484139
xyzuniverse <dev.xyzuniverse@gmail.com>
2024-01-13 09:01:53 +0700

    topaz: Add missing NFC Packages
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- device.mk
+++ device.mk
@@ -364,6 +364,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service.st \
     android.hardware.secure_element@1.2.vendor \
+    com.android.nfc_extras \
+    NfcNci \
+    SecureElement \
     libchrome.vendor \
     Tag
 
