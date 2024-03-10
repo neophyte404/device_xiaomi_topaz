76c01813364addeab5b9b2cf6bf555b9884aa538
luk1337 <priv.luk@gmail.com>
2024-03-10 16:28:33 +0300

    topaz: Configure aux camera for Aperture

--- device.mk
+++ device.mk
@@ -387,7 +387,8 @@ PRODUCT_PACKAGES += \
     TelecommResCommon \
     TelephonyResCommon \
     WifiResCommon \
-    WifiResTarget
+    WifiResTarget \
+    ApertureTopaz
 
 # Overlays Lineage
 DEVICE_PACKAGE_OVERLAYS += \
