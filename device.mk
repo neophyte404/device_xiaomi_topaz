0daac9747c48f21eb5e6f7ac03f1e777a12749d0
neophyte404 <260553983+neophyte404@users.noreply.github.com>
2026-06-08 04:11:38 +0000

    topaz: Include MiuiCamera

--- device.mk
+++ device.mk
@@ -16,6 +16,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 # Enable virtual AB with vendor ramdisk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
+# Miui Camera
+$(call inherit-product, vendor/xiaomi/miuicamera/config.mk)
+
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,3 +3,6 @@
 # Override host metadata to make builds more reproducible and avoid leaking info
 export BUILD_USERNAME=neophyte
 export BUILD_HOSTNAME=neophyte-build
+
+# MiuiCamera
+git clone --depth=1 https://github.com/neophyte404/vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
