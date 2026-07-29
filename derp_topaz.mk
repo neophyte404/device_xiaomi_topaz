40b61bab712b9e681738368c123333ffe1e43ccd
neophyte404 <260553983+neophyte404@users.noreply.github.com>
2026-07-30 00:20:05 +0700

    topaz: Derpfest-Ify

--- derp_topaz.mk
+++ derp_topaz.mk
@@ -11,14 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from topaz device
 $(call inherit-product, device/xiaomi/topaz/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-# Gapps Core
-WITH_GMS := true
+# Inherit some common Derpfest stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Device identifier
-PRODUCT_NAME := lineage_topaz
+PRODUCT_NAME := derp_topaz
 PRODUCT_DEVICE := topaz
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
